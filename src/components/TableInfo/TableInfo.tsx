ffb9e0d992eb876f861adf92bf9425d159e57cd0
ravel01106 <amandaravelocabrera@gmail.com>
2024-07-08 22:51:31 +0100

    feat: the content inside the thbody scrolls when it reaches a certain height

--- src/components/TableInfo/TableInfo.tsx
+++ src/components/TableInfo/TableInfo.tsx
@@ -5,32 +5,37 @@ import style from "./TableInfo.module.css"
 const TableInfo = () => {
   return (
     <div className="container mt-5">
-    <table className="table table-hover">
-      <thead className="table-dark">
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Pet Name</th>
-          <th scope="col">Owner Name</th>
-          <th scope="col">Date</th>
-          <th scope="col">Symptoms</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {dates.map(date  => (
-          <tr className={`${style.field}`}>
-          <th scope="row">{date.Id}</th>
-          <td>{date.petName}</td>
-          <td>{date.ownerName}</td>
-          <td>{date.date + " - " + date.time}</td>
-          <td>{date.symptoms}</td>
-          <td><button type="button" className={`btn btn-primary text-light ${style.icon}`}><FaEye size={24} /></button></td>
-        </tr>
-        ))}
+      <div className={`table-responsive ${style.tableScroll}`}>
+        <table className="table table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Pet Name</th>
+              <th scope="col">Owner Name</th>
+              <th scope="col">Date</th>
+              <th scope="col">Symptoms</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {dates.map(date => (
+              <tr className={`${style.field}`}>
+                <th scope="row">{date.Id}</th>
+                <td>{date.petName}</td>
+                <td>{date.ownerName}</td>
+                <td>{date.date + " - " + date.time}</td>
+                <td>{date.symptoms}</td>
+                <td><button type="button" className={`btn btn-primary text-light ${style.icon}`}><FaEye size={24} /></button></td>
+              </tr>
+            ))}
 
-      </tbody>
-    </table>
-  </div>
+          </tbody>
+        </table>
+
+
+      </div>
+
+    </div>
   )
 }
 
--- src/data/dates.ts
+++ src/data/dates.ts
@@ -50,6 +50,55 @@ const dates:IDates[] = [
     time: "08:00",
     symptoms: "broken left leg",
 },
+{
+    Id:"1",
+    petName: "Goku",
+    ownerName: "Adrian Gonzalez",
+    date: "04/02/2024",
+    time: "12:45",
+    symptoms: "stomach pain",
+},
+{
+    Id:"2",
+    petName: "Harry",
+    ownerName: "Amanda Perez",
+    date: "12/06/2024",
+    time: "11:15",
+    symptoms: "diarrhoea",
+},
+{
+
+    Id:"3",
+    petName: "Bolt",
+    ownerName: "David Flores",
+    date: "15/03/2024",
+    time: "15:30",
+    symptoms: "annual review",
+},
+{
+    Id:"4",
+    petName: "Gojo",
+    ownerName: "Maria Lopez",
+    date: "23/05/2024",
+    time: "09:15",
+    symptoms: "pain in the eyes",
+},
+{
+    Id:"5",
+    petName: "Bolt",
+    ownerName: "Oliver Flores",
+    date: "15/03/2023",
+    time: "10:15",
+    symptoms: "annual review",
+},
+{
+    Id:"6",
+    petName: "Freddie",
+    ownerName: "Marco Muñoz",
+    date: "30/12/2023",
+    time: "08:00",
+    symptoms: "broken left leg",
+},
 ]
 
 export default dates;
