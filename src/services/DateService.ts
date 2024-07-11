3be19b7b0ea96915ac6f9b894543be84346583a0
ravel01106 <amandaravelocabrera@gmail.com>
2024-07-11 11:56:03 +0100

    feat: created DateService service

---
+++ src/services/DateService.ts
@@ -0,0 +1,46 @@
+import dates from "../data/dates"
+import { IDates } from "../interfaces/IDates"
+
+const findAllDate = () => {
+  return dates
+}
+
+const findDateById = (id:string) => {
+  const dateById:IDates = dates.filter( date => date.Id === id)[0]
+  return dateById
+}
+
+
+const addNewDate = (newDate:IDates) => {
+  dates.push({
+    Id: (dates.length + 1).toString(),
+    petName: newDate.petName,
+    ownerName: newDate.ownerName,
+    date: newDate.date,
+    time: newDate.time,
+    symptoms: newDate.symptoms
+  });
+}
+
+
+const updateDateById = (date:IDates, id:string) => {
+
+}
+
+
+const deleteDateById = (id:string) => {
+  const dateToDelete:IDates = dates.filter( date => date.Id === id)[0]
+  delete dates[dates.indexOf(dateToDelete)]
+}
+
+
+
+const DateService = {
+  findAllDate,
+  findDateById,
+  addNewDate,
+  updateDateById,
+  deleteDateById
+}
+
+export default DateService;
